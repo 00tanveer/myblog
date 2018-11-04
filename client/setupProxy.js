04bfe38892c388e79792907b23740742b91fcec8
00tanveer <tanveer_fh09@yahoo.com>
2018-11-04 23:36:53 +0600

    added client

--- client/setupProxy.js
+++
@@ -1,6 +0,0 @@
-// const proxy = require("http-proxy-middleware");
-
-// module.exports = app => {
-//   console.log('yeayeayey');
-//   app.use(proxy("/", { target: "http://localhost:4001" }));
-// };
--- client/src/styles/theme.js
+++
@@ -1,8 +0,0 @@
-const theme = {
-  black: "black",
-  white: "white",
-  //maroon: "#872e2e"
-  maroon: "#cc0033"
-};
-
-export default theme;
