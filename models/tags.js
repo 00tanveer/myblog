c74b2b871510a944e236728b3661e099b14abd66
00tanveer <tanveer_fh09@yahoo.com>
2018-11-04 23:21:42 +0600

    added tags

--- models/tags.js
+++ models/tags.js
@@ -1,7 +1,10 @@
 module.exports = {
-  fashion: ["summer", "shoes"],
-  beauty: ["makeup", "skin care"],
+  fashion: ['summer', 'winter', 'dresses', 'tops', 'pants', "shoes", 'heels', 'boots',
+            'blouse', 'saree', 'formal', 'informal'],
+  beauty: ["makeup", 'eyeshadow', 'lipstick', 'foundation', 'concealer', 'blush', 'lip gloss', 
+          'eye liner', 'bronzer', 'contour', 'eyebrows', 'oils', 'lotion', 'face wash', 
+          'face masks', 'scrubs', 'face creams', 'nail polish', "skin care"],
   art: ["sketches", "pen"],
-  books: ["review"],
-  lifestyle: ["travel", "cooking"]
+  books: ["sci-fi", 'thriller', 'horror', 'romance', 'young adult', 'fantasy', 'history'],
+  lifestyle: ["travel", "cooking", 'pets', 'relationships']
 };
