4d8cdc2914459d32c736384e8a3609b6f37b572b
00tanveer <tanveer_fh09@yahoo.com>
2018-11-06 00:28:19 +0600

    added client

--- client/src/components/ui/engagement/Engagement.js
+++ client/src/components/ui/engagement/Engagement.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import theme from '../../../styles/theme';
+
+const StyledContainer = styled.div`
+	margin: 20px 20% 20px 20%;
+	button {
+		color: white;
+		cursor: pointer;
+		margin: 10px;
+		padding: 5px 9px;
+		background-color: ${theme.maroon};
+		border: 1px solid ${theme.white};
+		border-radius: 10px;
+		transition: all 200ms ease-in;
+		&:hover {
+			background-color: ${theme.black};
+			color: ${theme.white};
+			transform: scale(1.1);
+		}
+	}
+`;
 
 const Engagement = () => {
 	return(
-		<div>Hello</div>
+		<StyledContainer>
+			<button>Like</button>
+			<span>•</span>
+			<button>Comment</button>
+			<span>•</span>
+			<button><i className="fab fa-facebook"/> Share</button>
+		</StyledContainer>
 	);
 }
 
